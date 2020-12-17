02ef4bebd4c26b9595cb42618625c284196e5001
pladan <piotr.ladan@awacademyconsultant.de>
2020-12-17 10:55:51 +0100

    second another change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code her
         System.out.println("hello world");
         System.out.println("something else");
+        System.out.println("Another change as this programme is freaking awesome");
     }
 }
