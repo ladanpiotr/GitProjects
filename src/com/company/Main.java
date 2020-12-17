4548673877d845082ac7b8f56151f836dfcff514
Ładan Piotr <Piotr.Ladan@t-mobile.pl>
2020-12-17 10:14:43 +0100

    added greetings

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code her
+        System.out.println("hello world");
+        System.out.println("something else");
+    }
+}
